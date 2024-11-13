25f05b3991d5ff437b8df9e7aca84c6b61d77d67
satisfy <104036234+satisfyu@users.noreply.github.com>
2024-11-13 06:19:52 +0100

    feat(fix): Issue #296
    https://github.com/satisfyu/Let-s-Do-Hub/issues/296

--- common/src/main/java/net/satisfy/vinery/entity/TraderMuleEntity.java
+++ common/src/main/java/net/satisfy/vinery/entity/TraderMuleEntity.java
@@ -1,5 +1,6 @@
 package net.satisfy.vinery.entity;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
@@ -8,16 +9,20 @@ import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.animal.horse.AbstractChestedHorse;
+import net.minecraft.world.entity.npc.WanderingTrader;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.satisfy.vinery.registry.EntityRegistry;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 public class TraderMuleEntity extends AbstractChestedHorse {
 	public TraderMuleEntity(EntityType<? extends TraderMuleEntity> entityType, Level world) {
 		super(entityType, world);
 	}
+	private int despawnDelay = 47999;
 
 	@Override
 	protected void registerGoals() {
@@ -28,6 +33,44 @@ public class TraderMuleEntity extends AbstractChestedHorse {
 		this.goalSelector.addGoal(5, new TemptGoal(this, 1.0, Ingredient.of(Items.HAY_BLOCK), false));
 		this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0));
 		this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+	}
+
+	public void aiStep() {
+		super.aiStep();
+		if (!this.level().isClientSide) {
+			this.maybeDespawn();
+		}
+
+	}
+
+	public void addAdditionalSaveData(CompoundTag compoundTag) {
+		super.addAdditionalSaveData(compoundTag);
+		compoundTag.putInt("DespawnDelay", this.despawnDelay);
+	}
+	public void readAdditionalSaveData(CompoundTag compoundTag) {
+		super.readAdditionalSaveData(compoundTag);
+		if (compoundTag.contains("DespawnDelay", 99)) {
+			this.despawnDelay = compoundTag.getInt("DespawnDelay");
+		}
+	}
+
+	private void maybeDespawn() {
+		if (this.canDespawn()) {
+			this.despawnDelay = this.isLeashedToWanderingTrader() ? ((WanderingTrader) Objects.requireNonNull(this.getLeashHolder())).getDespawnDelay() - 1 : this.despawnDelay - 1;
+			if (this.despawnDelay <= 0) {
+				this.dropLeash(true, false);
+				this.discard();
+			}
+		}
+	}
+	private boolean canDespawn() {
+		return !this.isTamed() && !this.isLeashedToSomethingOtherThanTheWanderingTrader() && !this.hasExactlyOnePlayerPassenger();
+	}
+	private boolean isLeashedToWanderingTrader() {
+		return this.getLeashHolder() instanceof WanderingWinemakerEntity;
+	}
+	private boolean isLeashedToSomethingOtherThanTheWanderingTrader() {
+		return this.isLeashed() && !this.isLeashedToWanderingTrader();
 	}
 
 	@Override
